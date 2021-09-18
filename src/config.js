6895a966b7dd8ec57a13d730ca4d7c31cad7bfb2
lucumon <vacamaribu@hotmail.es>
2021-09-18 19:11:01 +0200

    Test brand map

--- src/config.js
+++ src/config.js
@@ -1734,6 +1734,38 @@ var vectorLayer = new ol.layer.Vector({
 			}
 		},
 		{
+			group: 'Name Suggestion Index',
+			title: 'Brands',
+			query: '(nwr[~"^brand$"~"."]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'base/circle.svg',
+			iconStyle: 'background-color:#0000ff',
+			style: function (feature) {
+				var key_regex = /^name:194[0-9]$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(0,0,255,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: '#0000ff',
+					width: 1.25
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Circle({
+						fill: fill,
+						stroke: stroke,
+						radius: 5
+					}),
+							text: new ol.style.Text({
+								text: name
+							}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+		},
+		{
 			group: 'Decades',
 			title: '1950-1959',
 			query: '(nwr[~"^name:195[0-9]$"~"."]({{bbox}});node(w););out meta;',
